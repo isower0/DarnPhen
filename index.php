a970dde77b7cd76c0f85923e43ae2b03bd9d1a5a
isower0 <58isorn@gmail.com>
2021-01-27 00:05:56 +0700

    index

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+<ul>
+ <li><a href="dbconfig.php">dbconfig.php</a></li>
+  <li><a href="market.php">market.php</a></li>
+  <li><a href="add_description.php">add_description.php</a></li>
+  <li><a href="add_product.php">add_product.php</a></li>
+  <li><a href="add_user_market.php">add_user_market.php</a></li>
+  <li><a href="bank_market_name.php">bank_market_name.php</a></li>
+  <li><a href="Bank_market.php">Bank_market.php</a></li>
+  <li><a href="cancel_order.php">cancel_order.php</a></li>
+  <li><a href="cart.php">cart.php</a></li>
+</ul>
+<a href="img.php">img show</a>
+<ul>
+
+</ul>
+</body>
+</html>
